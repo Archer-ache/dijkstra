8c839b02e44b2193d81f7a067b3a12548356e735
Archer <dupengche@gmail.com>
2022-12-06 18:42:56 +0800

    data-structure

---
+++ lab2.cpp
@@ -0,0 +1,10 @@
+//**导航软件--用dijkstra算法求最短路径**
+
+//附加实现内容：稀疏图存储结构
+//附加实现内容：堆优化的dijkstra算法
+//--默认为有向图
+#define INF INT_MAX
+#define MAX_VERTEX_NUM  50
+#include <iostream>
+#include <cmath>
+using namespace std;
