d01f06f4d2688c4a7b15f70fe0dd78a79ff0a9eb
Archer <dupengche@gmail.com>
2022-12-07 17:55:47 +0800

    test

--- lab2.cpp
+++ lab2.cpp
@@ -4,7 +4,28 @@
 //附加实现内容：堆优化的dijkstra算法
 //--默认为有向图
 #define INF INT_MAX
-#define MAX_VERTEX_NUM  50
+#define MAX_VERTEX_NUM  150000
 #include <iostream>
+#include <stdio.h>
 #include <cmath>
+//
 using namespace std;
+typedef struct{
+    int vexs[MAX_VERTEX_NUM];
+    int ADJmatrix[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
+    int vexnum,arcnum;
+}MGraph;
+void CreateMGraph(MGraph &G){
+    freopen("data.txt","r",stdin);
+    int n=4;
+    int a,b,c
+    while(!n){
+        cin>>a>>b;
+        cout<<a<<b<<c<<endl;
+        n--;
+    }
+}
+int main(){
+
+    CreateMGraph();
+}
